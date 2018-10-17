becd294e9437ba9207d1aa705b10ef128bc9e89a
Gatdev <giovannigatfut@gmail.com>
2018-10-17 15:06:45 -0500

    numeros pares e impares

---
+++ paroimpar.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 17 15:03:11 2018
+
+@author: giovannisilva
+"""
+number = 0
+number = int(input("Dame un número: "))
+if number%2 == 0 :
+    print("el número:", number, "Es par")
+else:
+    print("el número", number, "Es impar")
