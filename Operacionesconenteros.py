e30ed5d1e0fd779c8c57340c1eeaff035ea7e13b
Gatdev <giovannigatfut@gmail.com>
2018-11-04 20:07:29 -0600

    Operaciones con enteros

---
+++ Operacionesconenteros.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  4 19:51:12 2018
+
+@author: giovannisilva
+"""
+#Dado un número dar el triple
+n=3 
+print("El triple de: "+str(n)+" es: "+str(n*3))
+#Elevar a potencia 
+print("El cubo de: "+str(n)+" es: "+str(n**3))
+print("El cuadrado de: "+str(n)+" es: "+str(pow(n,2)))
+g=2
+k=8
+l=(g+k)**2 #Variable auxiliar 
+#l=pow((g+k),2)
+print("El resultado es: "+str(l))
+m=3
+n=4
+h=((m+n)/(2*n))+(m+n)*(m+n)
+print("El resultado es: "+str(h))
