e674c553eb0bd44dae64f4107577cd1ed44cc4aa
Gatdev <giovannigatfut@gmail.com>
2018-11-04 21:43:35 -0600

    Variable auxiliar(Cambio de valor)

---
+++ Variable_auxiliar.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  4 21:19:49 2018
+
+@author: giovannisilva
+"""
+
+e=10
+f=20
+print("Valores iniciales\n")
+print("El valor de e: "+str(e))
+print("El valor de f: "+str(f))
+g=e
+e=f
+f=g
+print("\n Valores finales\n")
+print("El valor de e: "+str(e))
+print("El valor de f: "+str(f))
